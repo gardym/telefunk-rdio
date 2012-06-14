d540087924f3abf7c8f2594f0f50e5f11396ed8a
Mike Gardiner <conversationing@gmail.com>
2012-06-14 23:49:22 +0800

    Unshift some paths to make loading easier.

--- get_collection.rb
+++ get_collection.rb
@@ -1,4 +1,7 @@
-require File.join(File.dirname(__FILE__), 'vendor', 'rdio-simple', 'ruby', 'rdio')
-require File.join(File.dirname(__FILE__), 'api_key')
+$:.unshift(File.dirname(__FILE__))
+$:.unshift(File.join(File.dirname(__FILE__), 'vendor', 'rdio-simple', 'ruby'))
+
+require 'vendor/rdio-simple/ruby/rdio'
+require 'api_key'
 
 rdio = Rdio.new([API_KEY[:key], API_KEY[:secret]])

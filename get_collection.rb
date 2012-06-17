19153cc60a738c9f74ae370916c25fe9e6656f73
Mike Gardiner <conversationing@gmail.com>
2012-06-17 12:23:56 +0800

    Remove get_collection and add in a console client for determining if an artist is on Rdio

--- get_collection.rb
+++
@@ -1,7 +0,0 @@
-$:.unshift(File.dirname(__FILE__))
-$:.unshift(File.join(File.dirname(__FILE__), 'vendor', 'rdio-simple', 'ruby'))
-
-require 'vendor/rdio-simple/ruby/rdio'
-require 'api_key'
-
-rdio = Rdio.new([API_KEY[:key], API_KEY[:secret]])
